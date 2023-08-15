5f6a9e93f2af9f823ae5deee52990ac261f0da13
ruu23 <arwamah777@gmail.com>
2023-08-15 08:49:20 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void jack_bauer(void){
+     {
+    for (int hour = 0; hour < 24; hour++) {
+        for (int minute = 0; minute < 60; minute++) {
+            printf("%02d:%02d\n", hour, minute);
+        }
+    }
+}
+}
+int main(void)
+{
+    jack_bauer();
+    return 0;
+}
