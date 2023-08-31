a9ebcb8713ded913cddf6565b26b859343f14f6e
ruu23 <arwamah777@gmail.com>
2023-08-30 17:02:08 -0700

    <3

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,18 +4,34 @@
  * @n: parameter
  * Return: 1,0
  */
+/**
+ * is_divisible_by - function
+ * @n: 1
+ * @divisor: 2
+ * Return: 0
+ */
+int is_divisible_by(int n, int divisor)
+{
+if (divisor == 1)
+{
+return (1);
+}
+if (n % divisor == 0)
+{
+return (0);
+}
+return (is_divisible_by(n, divisor - 1));
+}
+
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; ++i)
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	return (1);
+if (n <= 1)
+{
+return (0);
+}
+if (n == 2)
+{
+return (1);
+}
+return (is_divisible_by(n, n - 1));
 }
