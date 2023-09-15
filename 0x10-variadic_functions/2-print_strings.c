260c5aa1790cf1232c3842af1c3d970160155f7d
ruu23 <arwamah777@gmail.com>
2023-09-15 07:21:18 -0700

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(args, const char *);
 		if (string == NULL)
 		{
-			printf ("(nil)");
+			printf("(nil)");
 		}
 		else
 		{
