4bdc03c1c3c42767600d5f3e990b901780b38823
ruu23 <arwamah777@gmail.com>
2023-10-04 05:48:52 +0000

    name of the file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+/**
+ * main - function.
+ * @argc: int.
+ * @argv: char.
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0 && argv[0])
 	{
 		printf("%s\n", argv[0]);
 	}
-	return 0;
+	return (0);
 }
