12703c4f6b541c83edb28501c7881490bc20b4e0
ruu23 <arwamah777@gmail.com>
2023-10-07 08:01:52 +0000

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0 alwayes (success)
+ */
+int main (int argc, char argv[])
+{
+	_putchar(argc);
+	_putchar('\n');
+	return (0);
+}
