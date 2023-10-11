78eeac2dff402d393194da93a69a694cb5d400a4
ruu23 <arwamah777@gmail.com>
2023-10-11 00:09:10 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type struct
+ * @name: char*
+ * @age: float
+ * @owner: char*
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
