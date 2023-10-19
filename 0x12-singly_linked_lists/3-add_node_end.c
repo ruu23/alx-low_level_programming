c9a855d4463011b1133665c6f0d589340b54a9b9
ruu23 <arwamah777@gmail.com>
2023-10-19 12:29:06 -0700

    insertion at the end of list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * add_node_end - insertion in the end of list
+ * @str: char
+ * @head: ptr
+ *
+ * Return: new_node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	return (new_node);
+}
