d7c8d71a0c77025320d330ec52e725658a7717ac
ruu23 <arwamah777@gmail.com>
2023-10-19 11:22:48 -0700

    number of element

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - print number of element
+ * @h: parameter
+ *
+ * Return: count
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
