36f4828423a0d9ff29f2fe98e36431a13a462bd1
ruu23 <arwamah777@gmail.com>
2023-10-20 02:32:00 -0700

    insertion in the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,6 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->len = strlen(str);
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
@@ -34,8 +35,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		new_node->next = *head;
-		*head = new_node;
+		list_t *current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
 	}
 	return (new_node);
 }
